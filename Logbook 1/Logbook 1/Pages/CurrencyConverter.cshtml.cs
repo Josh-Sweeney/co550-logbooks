8fe7f4ffd62aeb60f6c08b9dfe1f95135374d8d8
Josh-Sweeney <115168998+JSweeney-GHA@users.noreply.github.com>
2023-01-27 10:05:05 +0000

    Add Currency Converter Page

---
+++ Logbook 1/Logbook 1/Pages/CurrencyConverter.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Logbook_1.Pages;
+
+public class CurrencyConverter : PageModel
+{
+    private readonly decimal _exchangeRate = 1.14m;
+    
+    // The input currency as GBP
+    public decimal InputCurrency { get; set; }
+    
+    // The output currency as Euro
+    public decimal OutputCurrency { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public void OnPost()
+    {
+        InputCurrency = decimal.Parse(Request.Form["inputCurrency"]);
+        
+        OutputCurrency = InputCurrency * _exchangeRate;
+    }
+}
