1138089be734ca00bd199ec87efcfdf87c3d2b72
Josh Sweeney <jsweeney@ghasolutions.co.uk>
2023-02-10 11:21:58 +0000

    Add pagination to Students page

--- Logbook 3/Pages/Students/Index.cshtml.cs
+++ Logbook 3/Pages/Students/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Models;
+using Microsoft.Extensions.Configuration;
 using NuGet.Configuration;
 
 namespace ContosoUniversity.Pages.Students
@@ -11,26 +12,33 @@ namespace ContosoUniversity.Pages.Students
     public class IndexModel : PageModel
     {
         private readonly Data.SchoolContext _context;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(Data.SchoolContext context)
+        public IndexModel(Data.SchoolContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
-        
+
         public string NameSort { get; set; }
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get;set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
+            if (searchString != null)
+                pageIndex = 1;
+            else
+                searchString = CurrentFilter;
+
             CurrentFilter = searchString;
-            
             var studentsIQ = from s in _context.Students select s;
 
             if (!string.IsNullOrEmpty(searchString))
@@ -45,7 +53,10 @@ namespace ContosoUniversity.Pages.Students
                 _ => studentsIQ.OrderBy(s => s.LastName)
             };
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            var pageSize = _configuration.GetValue("PageSize", 4);
+
+            Students = await PaginatedList<Student>.CreateAsync(studentsIQ.AsNoTracking(), pageIndex ?? 1,
+                pageSize);
         }
     }
 }
