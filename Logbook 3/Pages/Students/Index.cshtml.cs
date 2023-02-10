6e910e1a07a919a995b25af5d7d3c63c801998e2
Josh Sweeney <jsweeney@ghasolutions.co.uk>
2023-02-10 11:12:19 +0000

    Add sorting to Students page

--- Logbook 3/Pages/Students/Index.cshtml.cs
+++ Logbook 3/Pages/Students/Index.cshtml.cs
@@ -1,28 +1,45 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Models;
+using NuGet.Configuration;
 
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
+        private readonly Data.SchoolContext _context;
 
-        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        public IndexModel(Data.SchoolContext context)
         {
             _context = context;
         }
+        
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public IList<Student> Student { get;set; } = default!;
+        public IList<Student> Students { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Students != null)
+            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            
+            var studentsIQ = from s in _context.Students select s;
+
+            studentsIQ = sortOrder switch
             {
-                Student = await _context.Students.ToListAsync();
-            }
+                "name_desc" => studentsIQ.OrderByDescending(s => s.LastName),
+                "Date" => studentsIQ.OrderBy(s => s.EnrollmentDate),
+                "date_desc" => studentsIQ.OrderByDescending(s => s.EnrollmentDate),
+                _ => studentsIQ.OrderBy(s => s.LastName)
+            };
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
