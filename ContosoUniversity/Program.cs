3fde5482b2800087c0d6e4c04d0009f376c84f44
Josh Sweeney <jsweeney@ghasolutions.co.uk>
2023-02-10 10:29:26 +0000

    Add Dbinitializer

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -20,8 +20,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     
     var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
-    //DbInitializer.Initialize(context);
+    DbInitializer.Initialize(context);
 }
 
 
